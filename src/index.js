4064770bb8f587e1f248918edcd23fe946953c5e
szumi112 <mattszumilo@gmail.com>
2022-04-05 06:18:09 -0400

    lul

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 
 
+/*
+
+"start": "react-scripts start",
+"build": "concurrently \"npm run server\" \"react-scripts build\",
+"test": "react-scripts test",
+"eject": "react-scripts eject",
+"server": "node public/index.js",
+"client": "npm start index.js",
+"dev": "concurrently \"npm run server\" \"npm run client\""
+
+*/
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
