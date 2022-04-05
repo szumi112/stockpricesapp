2b4a22023019e5d0d9eabb8a798f8a3c2f6cba92
szumi112 <mattszumilo@gmail.com>
2022-04-05 03:42:32 -0400

    progresss

--- public/index.js
+++ public/index.js
@@ -2,9 +2,29 @@ const request = require("request-promise");
 const fs = require("fs");
 const cheerio = require("cheerio");
 
+const http = require("http");
+
+// Port Environment variable
+const PORT = process.env.PORT || 5000;
+
+// Creating the node server
+const SERVER = http.createServer();
+
+// Firing up the server on selected port
+SERVER.listen(PORT);
+
+SERVER.on("listening", () => {
+    console.log("[Server]::LISTEN:%s", PORT);
+});
+
+// Callback function for checking connecting or error
+SERVER.on("error", error => {
+    throw new Error(`[Server]::ERROR:${error.message}`);
+});
+
 async function tsla() {
     const html = await request.get(
-        "https://finance.yahoo.com/quote/TSLA/"
+        "https://finance.yahoo.com/quote/BTC-USD/"
         );
 
     const $ = await cheerio.load(html);
@@ -12,9 +32,9 @@ async function tsla() {
     const Price = $('[class="Fw(b) Fz(36px) Mb(-4px) D(ib)"]').text();
     const Symbol = $('[class="D(ib) Fz(18px)"]').text();
     const PercentChange = $('fin-streamer[class="Fw(500) Pstart(8px) Fz(24px)"][data-field="regularMarketChangePercent"]').find('span').text();
-    fs.writeFileSync("./tslaSymbol.csv", Symbol);
-    fs.writeFileSync("./tslaPrice.csv", Price);
-    fs.writeFileSync("./tslaPercentChange.csv", PercentChange)
+    fs.writeFileSync("./public/tslaSymbol.csv", Symbol);
+    fs.writeFileSync("./public/tslaPrice.csv", Price);
+    fs.writeFileSync("./public/tslaPercentChange.csv", PercentChange)
     
         console.log(Price)
     setTimeout(tsla, 20000); //60 seconds == 1minute
@@ -32,9 +52,9 @@ async function aapl() {
     const Price = $('[class="Fw(b) Fz(36px) Mb(-4px) D(ib)"]').text();
     const Symbol = $('[class="D(ib) Fz(18px)"]').text();
     const PercentChange = $('fin-streamer[class="Fw(500) Pstart(8px) Fz(24px)"][data-field="regularMarketChangePercent"]').find('span').text();
-    fs.writeFileSync("./aaplSymbol.csv", Symbol);
-    fs.writeFileSync("./aaplPrice.csv", Price);
-    fs.writeFileSync("./aaplPercentChange.csv", PercentChange)
+    fs.writeFileSync("./public/aaplSymbol.csv", Symbol);
+    fs.writeFileSync("./public/aaplPrice.csv", Price);
+    fs.writeFileSync("./public/aaplPercentChange.csv", PercentChange)
     
 
     console.log(Price)
@@ -54,9 +74,9 @@ async function spy() {
     const Price = $('[class="Fw(b) Fz(36px) Mb(-4px) D(ib)"]').text();
     const Symbol = $('[class="D(ib) Fz(18px)"]').text();
     const PercentChange = $('fin-streamer[class="Fw(500) Pstart(8px) Fz(24px)"][data-field="regularMarketChangePercent"]').find('span').text();
-    fs.writeFileSync("./spySymbol.csv", Symbol);
-    fs.writeFileSync("./spyPrice.csv", Price);
-    fs.writeFileSync("./spyPercentChange.csv", PercentChange)
+    fs.writeFileSync("./public/spySymbol.csv", Symbol);
+    fs.writeFileSync("./public/spyPrice.csv", Price);
+    fs.writeFileSync("./public/spyPercentChange.csv", PercentChange)
 
     console.log(Price)
     setTimeout(spy, 20000); // 60,000 = 60 seconds == 1minute
@@ -74,9 +94,9 @@ async function nvda() {
     const Price = $('[class="Fw(b) Fz(36px) Mb(-4px) D(ib)"]').text();
     const Symbol = $('[class="D(ib) Fz(18px)"]').text();
     const PercentChange = $('fin-streamer[class="Fw(500) Pstart(8px) Fz(24px)"][data-field="regularMarketChangePercent"]').find('span').text();
-    fs.writeFileSync("./nvdaSymbol.csv", Symbol);
-    fs.writeFileSync("./nvdaPrice.csv", Price);
-    fs.writeFileSync("./nvdaPercentChange.csv", PercentChange)
+    fs.writeFileSync("./public/nvdaSymbol.csv", Symbol);
+    fs.writeFileSync("./public/nvdaPrice.csv", Price);
+    fs.writeFileSync("./public/nvdaPercentChange.csv", PercentChange)
 
     console.log(Price)
     setTimeout(nvda, 20000); // 60,000 = 60 seconds == 1minute
